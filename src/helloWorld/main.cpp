e38f76baf45a156beb6e3cc99435f1fae0adf506
thor <send.tilmig@gmail.com>
2019-07-31 00:16:46 +0200

    switching to vs code

--- src/helloWorld/main.cpp
+++ src/helloWorld/main.cpp
@@ -1,10 +1,16 @@
+/*
+* @Author: thor
+* @Date:   2019-07-27 16:00:20
+* @Last Modified by:   thor
+* @Last Modified time: 2019-07-30 22:57:19
+*/
 #include <iostream>
 
 
 int main(int argc, char const *argv[])
 {
 
-  std::cout << "Hello world! nowwwwww" << std::endl;
+  std::cout << "Hello cpp world! " << std::endl;
   return 0;
 }
 
